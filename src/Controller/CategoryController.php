86cab6178e7bf0641870028bac9a43e4de12f99b
richardR144 <richard.rigollet@lapiscine.pro>
2025-04-29 14:12:37 +0200

    les blocks

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -16,22 +16,22 @@ class CategoryController extends AbstractController
 			1 => [
 				'id' => 1,	
 				'nom' => 'Cocktails',
-				'description' => 'Les cocktails classiques sont des boissons alcoolisées qui ont résisté à l’épreuve du temps et qui sont devenues des incontournables dans le monde de la mixologie. Ils sont souvent préparés avec des ingrédients simples et sont appréciés pour leur goût équilibré et leur présentation élégante.'
+				'description' => 'Les cocktails classiques sont des boissons alcoolisées qui ont résisté à l’épreuve du temps et qui sont devenues des incontournables dans le monde de la mixologie.'
 			],
 			2 => [
 				'id' => 2,
-				'nom' => 'mocktails',
-				'description' => 'Les cocktails modernes sont des créations contemporaines qui reflètent les tendances actuelles en matière de mixologie. Ils peuvent inclure des ingrédients innovants, des techniques de préparation uniques et des présentations artistiques.'
+				'nom' => 'Mocktails',
+				'description' => 'Les cocktails modernes sont des créations contemporaines qui reflètent les tendances actuelles en matière de mixologie.'
 			],
 			3 => [
 				'id' => 3,
 				'nom' => 'Cocktails soft',
-				'description' => 'Les cocktails sans alcool, également appelés mocktails, sont des boissons qui imitent les cocktails traditionnels mais sans l’alcool. Ils sont parfaits pour ceux qui souhaitent profiter de saveurs complexes et rafraîchissantes sans les effets de l’alcool.'
+				'description' => 'Les cocktails sans alcool, également appelés mocktails, sont des boissons qui imitent les cocktails traditionnels mais sans l’alcool.'
 			],
 			4 => [
 				'id' => 4,
-				'nom' => 'shooter',
-				'description' => 'Les cocktails fruités sont des boissons qui mettent en avant les saveurs naturelles des fruits. Ils peuvent être préparés avec des jus de fruits frais, des purées de fruits ou des liqueurs fruitées, offrant une expérience gustative rafraîchissante et sucrée.'
+				'nom' => 'Shooter',
+				'description' => 'Les cocktails fruités sont des boissons qui mettent en avant les saveurs naturelles des fruits.'
 			],
 		];
 
