abf11837d2eb83fa8146b2e7961de89d192c4a34
richardR144 <richard.rigollet@lapiscine.pro>
2025-04-29 12:05:13 +0200

    CategoryController.php

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class CategoryController extends AbstractController
+{
+    #[Route('/categories', name: 'list-categories')]
+    public function displayListCategories(): Response
+    {
+        $categories = [
+			1 => [
+				'id' => 1,	
+				'nom' => 'Cocktails classiques',
+				'description' => 'Les cocktails classiques sont des boissons alcoolisées qui ont résisté à l’épreuve du temps et qui sont devenues des incontournables dans le monde de la mixologie. Ils sont souvent préparés avec des ingrédients simples et sont appréciés pour leur goût équilibré et leur présentation élégante.'
+			],
+			2 => [
+				'id' => 2,
+				'nom' => 'Cocktails modernes',
+				'description' => 'Les cocktails modernes sont des créations contemporaines qui reflètent les tendances actuelles en matière de mixologie. Ils peuvent inclure des ingrédients innovants, des techniques de préparation uniques et des présentations artistiques.'
+			],
+			3 => [
+				'id' => 3,
+				'nom' => 'Cocktails sans alcool',
+				'description' => 'Les cocktails sans alcool, également appelés mocktails, sont des boissons qui imitent les cocktails traditionnels mais sans l’alcool. Ils sont parfaits pour ceux qui souhaitent profiter de saveurs complexes et rafraîchissantes sans les effets de l’alcool.'
+			],
+			4 => [
+				'id' => 4,
+				'nom' => 'Cocktails fruités',
+				'description' => 'Les cocktails fruités sont des boissons qui mettent en avant les saveurs naturelles des fruits. Ils peuvent être préparés avec des jus de fruits frais, des purées de fruits ou des liqueurs fruitées, offrant une expérience gustative rafraîchissante et sucrée.'
+			],
+		];
+
+        return $this->render('category.html.twig', [
+            'categories' => $categories
+        ]);
+    }   
+}
--- src/Controller/CocktailController.php
+++ src/Controller/CocktailController.php
@@ -176,10 +176,44 @@ class CocktailController extends AbstractController
 			],
 		];
 
-        $cocktail = $cocktails[$id];
+		$cocktail = $cocktails[$id];
         
         return $this->render('single-cocktail.html.twig', [
             'cocktail' => $cocktail
         ]);
-    }
-} 
+	}
+		
+		#[Route('/categories', name: 'list-categories')]
+		public function displayListCategories(): Response
+	{
+		
+		$categories = [
+			1 => [
+				'id' => 1,	
+				'nom' => 'Cocktails',
+				'description' => 'Les cocktails classiques sont des boissons alcoolisées qui ont résisté à l’épreuve du temps et qui sont devenues des incontournables dans le monde de la mixologie. Ils sont souvent préparés avec des ingrédients simples et sont appréciés pour leur goût équilibré et leur présentation élégante.'
+			],
+			2 => [
+				'id' => 2,
+				'nom' => 'mocktail',
+				'description' => 'Les cocktails modernes sont des créations contemporaines qui reflètent les tendances actuelles en matière de mixologie. Ils peuvent inclure des ingrédients innovants, des techniques de préparation uniques et des présentations artistiques.'
+			],
+			3 => [
+				'id' => 3,
+				'nom' => 'Cocktails soft',
+				'description' => 'Les cocktails sans alcool, également appelés mocktails, sont des boissons qui imitent les cocktails traditionnels mais sans l’alcool. Ils sont parfaits pour ceux qui souhaitent profiter de saveurs complexes et rafraîchissantes sans les effets de l’alcool.'
+			],
+			4 => [
+				'id' => 4,
+				'nom' => 'shooter',
+				'description' => 'Les cocktails fruités sont des boissons qui mettent en avant les saveurs naturelles des fruits. Ils peuvent être préparés avec des jus de fruits frais, des purées de fruits ou des liqueurs fruitées, offrant une expérience gustative rafraîchissante et sucrée.'
+			]
+		];
+
+		$categorie = $categories;
+
+		return $this->render('list-categories.html.twig', [
+			'categories' => $categories
+		]);
+	}	    
+}
