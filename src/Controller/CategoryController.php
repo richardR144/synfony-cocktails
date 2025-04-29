15e80413a8538a36c2bdd17898aa63ba65d8fa37
richardR144 <richard.rigollet@lapiscine.pro>
2025-04-29 16:08:01 +0200

    affiche qu'un seul cocktail

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -38,5 +38,41 @@ class CategoryController extends AbstractController
         return $this->render('list-categories.html.twig', [
             'categories' => $categories
         ]);
-    }   
+    }
 }
+	
+
+
+class ShowCategoryController extends AbstractController
+{
+    #[Route('/categories/{$id}', name: 'show-categories')]
+    public function displayShowCategories($id): Response
+    {
+        $categories = [
+            1 => [
+                'id' => 1,	
+                'nom' => 'Cocktails',
+                'description' => 'Les cocktails classiques sont des boissons alcoolisées qui ont résisté à l’épreuve du temps et qui sont devenues des incontournables dans le monde de la mixologie.'
+            ],
+            2 => [
+                'id' => 2,
+                'nom' => 'Mocktails',
+                'description' => 'Les cocktails modernes sont des créations contemporaines qui reflètent les tendances actuelles en matière de mixologie.'
+            ],
+            3 => [
+                'id' => 3,
+                'nom' => 'Cocktails soft',
+                'description' => 'Les cocktails sans alcool, également appelés mocktails, sont des boissons qui imitent les cocktails traditionnels mais sans l’alcool.'
+            ],
+            4 => [
+                'id' => 4,
+                'nom' => 'Shooter',
+                'description' => 'Les cocktails fruités sont des boissons qui mettent en avant les saveurs naturelles des fruits.'
+            ],
+        ];
+
+        return $this->render('single-categories.html.twig', [
+            'categories' => $categories[$id],
+        ]);
+    }   
+} 
--- src/Controller/CocktailController.php
+++ src/Controller/CocktailController.php
@@ -215,5 +215,36 @@ class CocktailController extends AbstractController
 		return $this->render('list-categories.html.twig', [
 			'categories' => $categories
 		]);
-	}	    
+	}
+	
+	#[Route('/categories/{id}', name: 'show-categories')]
+	public function displayShowCategories($id): Response
+	{
+		$categories = [
+			1 => [
+				'id' => 1,	
+				'nom' => 'Cocktails',
+				'description' => 'Les cocktails classiques sont des boissons alcoolisées qui ont résisté à l’épreuve du temps et qui sont devenues des incontournables dans le monde de la mixologie.'
+			],
+			2 => [
+				'id' => 2,
+				'nom' => 'Mocktails',
+				'description' => 'Les cocktails modernes sont des créations contemporaines qui reflètent les tendances actuelles en matière de mixologie.'
+			],
+			3 => [
+				'id' => 3,
+				'nom' => 'Cocktails soft',
+				'description' => 'Les cocktails sans alcool, également appelés mocktails, sont des boissons qui imitent les cocktails traditionnels mais sans l’alcool.'
+			],
+			4 => [
+				'id' => 4,
+				'nom' => 'Shooter',
+				'description' => 'Les cocktails fruités sont des boissons qui mettent en avant les saveurs naturelles des fruits.'
+			],
+		];
+
+		return $this->render('show-categories.html.twig', [
+			'categorie' => $categories[$id]
+		]);
+	}		    
 }
