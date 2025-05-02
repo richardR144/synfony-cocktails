ccf8b048e605d36aa828ce8552414653ffbf91ed
richardR144 <richard.rigollet@lapiscine.pro>
2025-05-02 13:56:24 +0200

    création par php bin/console make:entity pour générer une nouvelle table Meal

---
+++ src/Entity/Meal.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity()]
+class Meal
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $recette = null;
+
+    #[ORM\Column]
+    private ?\DateTime $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function setId(string $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getRecette(): ?string
+    {
+        return $this->recette;
+    }
+
+    public function setRecette(string $recette): static
+    {
+        $this->recette = $recette;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTime $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
