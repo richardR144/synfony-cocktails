bb68e6c21e4385c831b983629fcb8a495152600f
richardR144 <richard.rigollet@lapiscine.pro>
2025-05-02 11:07:04 +0200

    php bin/console make:migration, php bin/console migrations:migrate

--- src/Entity/Cocktail.php
+++ src/Entity/Cocktail.php
@@ -1,10 +1,13 @@
 <?php
-
+//lors de l'examen, je peux expliquer la partie SQL avec cette exemplesans oublier de dire que l'on peut le faire 
+//à la main et  dans ce cas là Symfony le fait pour nous avec la ligne de commande : php bin/console make:migrations
+//Dans la classe Cocktail (dossier entity), rajoutez les annotations #[ORM] sur le nom de la classe (pour que 
+//symfony / doctrine puisse créer la table cocktail en BDD (et sur les propriétés id, name etc (pour que symfony / doctrine puisse créer les colonnes)
 namespace App\Entity;
 use App\Repository\CocktailsRepository;
 use Doctrine\ORM\Mapping as ORM;  //j'importe la classe ORM pour utiliser les annotations de Doctrine
 
-#[ORM\Entity(repositoryClass: CocktailsRepository::class)]
+#[ORM\Entity(repositoryClass: CocktailsRepository::class)]  //j'indique que c'est une entité de Doctrine et que son repository est CocktailsRepository
 class Cocktail
 {
     #[ORM\Id]  //j'indique que c'est la clé primaire de la table cocktails
@@ -36,3 +39,10 @@ class Cocktail
 
     }
 }
+
+
+//David: 1/ Dans la ligne de commande (terminal ou terminal de vs code), executez la ligne de commande : "php bin/console make:migration" 
+//pour demander à Symfony / Doctrine, de scanner vos annotations sur l'entité et de générer une requête SQL permettant de créer la table et les colonnes correspondantes.
+//David : 2/ Dans la ligne de commandes, executez la ligne de commandes : "php bin/console doctrine:migrations;migrate"
+//Ca va executer en BDD la requête SQL que vous avez généré à l'étape d'avant. Vérifiez en BDD que la table cocktail existe bien et que les colonnes ont bien été créées 
+//Si ça a fonnctionné, vous devriez voir un nouveau fichier "Versionxxxxxxxxx" dans le dossier migration, contenant une requête SQL
